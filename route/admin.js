e46c259813cc623069363a9aeec55db33b60dcd5
ruoyiyancheng <18768925223@163.com>
2019-12-26 16:34:02 +0800

    enter

--- route/admin.js
+++ route/admin.js
@@ -1,10 +1,14 @@
-//引入express框架
+//1 引入express框架
 const express = require('express');
-//创建钩子函数
+//2 创建钩子函数
 const admin = express.Router();
 
 admin.get('/login',(req,res) => {
     res.render('admin/login')
 });
-// 将路由对象作为成员模块进行导出
+//4 创建用户路由列表
+admin.get('/user',(req,res) => {
+    res.render('admin/user')
+});
+//3 将路由对象作为成员模块进行导出
 module.exports = admin;
