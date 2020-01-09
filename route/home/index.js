101d957832e1adcffa904f04d98bece966bd797f
ruoyiyancheng <18768925223@163.com>
2020-01-09 22:02:55 +0800

    当前已经完成了客户端的分页功能

--- route/home/index.js
+++ route/home/index.js
@@ -1,4 +1,16 @@
+const { Article } = require('../../model/article');
+
+const pagenation = require('mongoose-sex-page');
 //前台处理函数文件
-module.exports=  (req,res) => {
-        res.render('home/default.art');
+module.exports= async (req,res) => {
+    //获取页码值
+    const page = req.query.page;
+    //从数据库中查询数据
+    let result = await pagenation(Article).page(page).size(4).display(5).find().populate('author').exec();
+    // res.send(result);
+    // return;
+    //渲染模板并渲染数据
+    res.render('home/default.art',{
+        result: result
+    });
 }
