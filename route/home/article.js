170469fcae87709705ba9a2e52d687706ebddeb8
ruoyiyancheng <18768925223@163.com>
2020-01-31 14:25:44 +0800

    当前已经基本完成当前项目，后期手写一些功能用于对后期的项目进行完善

--- route/home/article.js
+++ route/home/article.js
@@ -1,5 +1,7 @@
 //导入文章集合构造函数
 const { Article } = require('../../model/article');
+//导入评论集合构造函数
+const { Comment } = require('../../model/comment');
 //用来处理文章详情
 module.exports= async (req,res) => {
     //接收客户端传递过来的文章的id值
@@ -7,10 +9,12 @@ module.exports= async (req,res) => {
 
     //根据id查询文章详细信息
     let article = await Article.findOne({_id: id}).populate('author');
-
-    // res.send(article);
+    let comments = await Comment.find({aid: id}).populate('uid');
+    // res.send(comments);
+    // return;
     res.render('home/article.art',{
-        article: article
+        article,
+        comments
     })
 }
 
