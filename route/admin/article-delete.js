e141e5b43f9fb0ad0c44bbe826f0c122f0397627
ruoyiyancheng <18768925223@163.com>
2020-03-22 21:14:12 +0800

    当前完成了文章的删除功能

--- route/admin/article-delete.js
+++ route/admin/article-delete.js
@@ -1,17 +1,9 @@
+//导入文章集合
 const { Article } = require('../../model/article');
-module.exports = async (req,res) => {
-    // await Article.findOneAndDelete({_id:req.query.id});
-    // res.redirect('/admin/article');
-    console.log(articles.records);
-    // res.send(articles.records);
-}
 
-// const { User } = require('../../model/user');
-// module.exports = async (req,res) => {
-//     // 获取要删除的用户id
-    
-//     //根据id删除用户
-//     await User.findOneAndDelete({_id:req.query.id});
-//     //将页面重定向到用户列表页面
-//     res.redirect('/admin/user');
-// }
+//创建删除功能函数
+module.exports = async (req,res) => {
+    await Article.findOneAndDelete({_id:req.query.id});
+    // res.send(result);
+    res.redirect('/admin/article');
+}
--- route/admin/user-delete.js
+++ route/admin/user-delete.js
@@ -1,7 +1,9 @@
 const { User } = require('../../model/user');
 module.exports = async (req,res) => {
     // 获取要删除的用户id
-    // res.send('ok');
+    //测试代码用来显示数据结构
+    // let resule = await User.find({_id: req.query.id});
+    // res.send(resule);
     //根据id删除用户
     await User.findOneAndDelete({_id:req.query.id});
     //将页面重定向到用户列表页面
